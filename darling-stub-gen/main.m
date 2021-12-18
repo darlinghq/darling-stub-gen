94b5d2084c83c49f0cc63fb585155728d6480cc4
Thomas A <curioustommy@protonmail.com>
2021-12-18 11:11:56 -0800

    Add main.m

---
+++ darling-stub-gen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  darling-stub-gen
+//
+//  Created by Thomas A. on 12/18/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
